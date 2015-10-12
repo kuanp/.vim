90344bf86ec49eb3dada3b87cc30bb0af930033e
Kuan <kuan@interana.com>
2015-10-12 15:45:58 -0700

    garbage cleanup

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 clear
 cp vimrc ~/.vimrc
-gunzip bundle.tar.gz && tar xvf bundle.tar
-gunzip change-solarized-color.tar.gz && tar xvf change-solarized-color.tar
+gunzip bundle.tar.gz && tar xvf bundle.tar && rm bundle.tar
+gunzip change-solarized-color.tar.gz && tar xvf change-solarized-color.tar && rm change-solarized-color.tar
